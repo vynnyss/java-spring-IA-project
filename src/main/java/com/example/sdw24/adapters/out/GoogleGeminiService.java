557f8547481086be7ca9bdfdbc470a93069b9586
Vinicius Sousa <vinissousa016@gmail.com>
2025-07-22 19:12:00 -0300

    update application.yml

--- src/main/java/com/example/sdw24/adapters/out/GoogleGeminiService.java
+++ src/main/java/com/example/sdw24/adapters/out/GoogleGeminiService.java
@@ -52,7 +52,7 @@ public interface GoogleGeminiService extends GenerativeAiService {
     class Config {
     	@Bean
     	public RequestInterceptor apiKeyRequestInterceptor(@Value("${gemini.api-key}") String apiKey) {
-    		return requestTemplate -> requestTemplate.query("key", apiKey);
+    		return requestTemplate -> requestTemplate.query("X-goog-api-key", apiKey);
     	}
     }
 }
